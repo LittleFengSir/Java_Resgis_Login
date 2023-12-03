2a58cdd5531c73b24a6d722b6857c52a3f42bc94
lfengsir <fengsir.hy@foxmail.com>
2023-12-03 15:48:48 +0800

    修复第一次验证失败，第二次验证成功后，无反应的bug

--- src/ChangeHandle.java
+++ src/ChangeHandle.java
@@ -11,21 +11,22 @@ public class ChangeHandle {
         this.changeView = changeView;
     }
     ActionListener confirmBtnListener = new ActionListener() {
-        boolean errorFlag = false;
+
         @Override
         public void actionPerformed(ActionEvent e) {
+            boolean errorFlag = false;
             boolean passwordMatch = passwordHasher.verifyPassword(new String(changeView.getOriginalPWDField().getPassword()),UserData.salt,UserData.userPWd);
-                 if (!changeView.getUserNameField().getText().equals(UserData.userName) || !passwordMatch){
-                     JOptionPane.showMessageDialog(null, "用户名或密码错误", "ERROR", JOptionPane.ERROR_MESSAGE);
-                     errorFlag = true;
-                 } else if (!Arrays.equals(changeView.getNewPWDField().getPassword(), changeView.getNewPWDField1().getPassword())) {
-                     JOptionPane.showMessageDialog(null, "两次密码不一样，请重新输入", "ERROR", JOptionPane.ERROR_MESSAGE);
-                     errorFlag = true;
-                 } else if (!changeView.getYzmField().getText().equals(changeView.codeGenerator.getGeneratedCode())) {
-                     JOptionPane.showMessageDialog(null, "验证码错误", "ERROR", JOptionPane.ERROR_MESSAGE);
-                     errorFlag = true;
-                 }
-                 if (!errorFlag){
+                if (!changeView.getUserNameField().getText().equals(UserData.userName) || !passwordMatch){
+                    JOptionPane.showMessageDialog(null, "用户名或密码错误", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    errorFlag = true;
+                } else if (!Arrays.equals(changeView.getNewPWDField().getPassword(), changeView.getNewPWDField1().getPassword())) {
+                    JOptionPane.showMessageDialog(null, "两次密码不一样，请重新输入", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    errorFlag = true;
+                } else if (!changeView.getYzmField().getText().equals(changeView.codeGenerator.getGeneratedCode())) {
+                    JOptionPane.showMessageDialog(null, "验证码错误", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    errorFlag = true;
+                }
+                if (!errorFlag){
                     UserData.userName = changeView.getUserNameField().getText();
                     byte[] salt = passwordHasher.generateSalt();
                     UserData.userPWd = passwordHasher.hashPassword(new String(changeView.getNewPWDField().getPassword()),salt);
